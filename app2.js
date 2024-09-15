86472ff8257add3ddac0739102ddf2487ec5b023
Olga <olgajura@mail.ru>
2024-09-15 11:49:46 +0700

    'delted'

--- app2.js
+++ app2.js
@@ -12,28 +12,4 @@ const app2 = Vue.createApp({
         changeTitle() {
             this.title = 'Love'
         }
-    },
-
-/*     template: 
-`
-        <div class="card center">
-            <h1>{{title}}</h1>
-            <button class="btn" @click="title = 'Другое'">Другое</button>
-        </div>
-    
-` */
-render() {
-    return h('div', {
-        class: 'card center'},
-    [
-        h('h1', {}, this.title),
-        h('button', {class: 'btn', onClick: this.changeTitle}, 'Other')
-    ])
-}, 
-beforeCreate() {
-    console.log('beforeCreate')
-},
-})
-
-
-app2.mount('#app')
+    },})
